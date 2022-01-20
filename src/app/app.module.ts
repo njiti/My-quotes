b7a84a26808e43ba13ca3e95ce2e3758fc5d2c9d
njiti <kamaukan77@gmail.com>
2022-01-20 13:10:39 -0930

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgxSmartModalModule } from 'ngx-smart-modal';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    NgxSmartModalModule.forRoot(),
+  ],
+  providers: [],
+  bootstrap: [ AppComponent ],
+})
+export class AppModule { }
